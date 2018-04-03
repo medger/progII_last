3cf6e65c0f54ddf95c3130ab9d20ba1dec95325f
Medina <dnt.1554@gmail.com>
2018-04-03 22:38:05 +0000

    trabalhoVitoria

--- trabalho_vitoria.c
+++ trabalho_vitoria.c
@@ -1,48 +1,24 @@
 /*
 Faça uma função que receba o tamanho de um cômodo em m² e o 
-tamanho do piso em cm² que será usado, e retorne a quantidade de pisos 
+tamanho do piso em m² que será usado, e retorne a quantidade de pisos 
 que será preciso para preencher todo o chão.
 */
-#include<stdio.h>
-float converteComodo (float comprimento, float largura);
-float convertePiso (float pcomprimento, float plargura);
+#include <stdio.h>
 float quantidade(float comodo, float piso);
 
 int main()
 {
-    float comprimento=0, largura=0, comodo=0;
-    float pcomprimento=0, plargura=0, piso=0;
-    float resp;
-        printf("Digite o COMPRIMENTOxLARGURA do cômodo em METROS:\n");
-        scanf("%f%f", &comprimento,&largura);
-            comodo=converteComodo(comprimento, largura);
-            printf("\nTamanho do cômodo em M²: %.2f", comodo);
-        
-        printf("\n\nEntre com COMPRIMENTOxLARGURA do PISO em CENTÍMETROS:\n");
-        scanf("%f%f", &pcomprimento,&plargura);
-            piso=convertePiso(pcomprimento,plargura);
-            printf("Tamanho do piso de CM para CM²: %.2f", piso);
-            
-            resp=quantidade(comodo,piso);
-        printf("\nPrecisará de %.2f pisos para preencher o comodo", resp);
-return 0;
+	float piso, comodo, total;
+	printf("Digite o tamanho do comodo em metros quadrados.");
+	scanf("%f", &comodo);
+	printf("Digite o tamanho do piso em metros quadrados.");
+	scanf("%f", &piso);
+    	total=quantidade(comodo, piso);
+	printf("A quantidade de pisos necessaria e: %.2f", total);
+	return 0;
 }
-
-float converteComodo (float comprimento, float largura)
-{
-    comprimento*=largura;
-    return comprimento;
-}
-
-float convertePiso (float pcomprimento, float plargura)
-{   
-    pcomprimento*=plargura; //converte CM para CM²
-    return pcomprimento;
-}
-
 float quantidade(float comodo, float piso)
 {
-    float aux=0;
-    comodo/=piso;
-    return comodo;
-}
+	float total=comodo/piso;
+	return total;
+} 
